54b8e147127504a2814e4d13cda1b855613644ac
Afool4U <2312163474@qq.com>
2021-08-16 07:20:45 +0800

    前两章

---
+++ 刷题/算法竞赛书/第一章 算法竞赛概述/hdu 1425.cpp
@@ -0,0 +1,29 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n';
+using namespace std;
+typedef long long ll;
+typedef stringstream sstream;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+int main() {
+	int n, m;
+	while (cin >> n >> m) {
+		int a[n];
+
+		for (auto& i : a) {
+			cin >> i;
+		}
+		sort(a, a + n, greater<int>());
+		for (size_t i = 0; i < m; i++) {
+			if (i)
+				cout << " ";
+			cout << a[i];
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
---
+++ 刷题/算法竞赛书/第一章 算法竞赛概述/线性算法时间认知.cpp
@@ -0,0 +1,19 @@
+﻿#include <bits/stdc++.h>
+
+#define endl '\n';
+using namespace std;
+typedef long long ll;
+typedef stringstream sstream;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+int main() {
+	int k = 0, n = 1e7;
+	clock_t start, end;
+	start = clock();
+	for (int i = 0;i < n;i++) k++;
+	end = clock();
+	cout << (double)(end - start) / CLOCKS_PER_SEC << endl;
+
+	return 0;
+}
