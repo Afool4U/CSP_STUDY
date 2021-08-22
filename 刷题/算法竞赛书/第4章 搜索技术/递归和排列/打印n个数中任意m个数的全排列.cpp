406bec2d75a8b0d91524777fb3dbb14f1ef15ebf
Afool4U <2312163474@qq.com>
2021-08-22 13:06:38 +0800

    8月22  (递归真琢磨不透诶)

---
+++ 刷题/算法竞赛书/第4章 搜索技术/递归和排列/打印n个数中任意m个数的全排列.cpp
@@ -0,0 +1,47 @@
+﻿/**
+ * @Author hjl
+ * @Date 2021年8月22日12:31:32
+ */
+
+#include <bits/stdc++.h>
+
+#define Swap(a,b) {int t=a;a=b;b=t;}
+#define endl '\n'
+#define ll long long
+#define sstream stringstream
+using namespace std;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+int a[15];
+int m;
+
+void Perm(int begin, int end) {
+	int i;
+	if (begin == m) {
+		for (int i = 0;i < m;i++) {
+			cout << a[i];
+		}
+		cout << endl;
+	}
+	else {
+		for (i = begin;i <= end;i++) {
+			Swap(a[begin], a[i]);
+			Perm(begin + 1, end);
+			Swap(a[begin], a[i]);
+		}
+	}
+}
+
+int main() {
+
+	int n;
+	cin >> n;
+	for (int i = 0;i < n;i++) {
+		cin >> a[i];
+	}
+	cin >> m;
+	Perm(0, n - 1);
+
+	return 0;
+}
---
+++ 刷题/算法竞赛书/第4章 搜索技术/递归和排列/递归求全排列个数.cpp
@@ -0,0 +1,41 @@
+﻿/**
+ * @Author hjl
+ * @Date 2021年8月22日11:44:28
+ */
+
+#include <bits/stdc++.h>
+
+#define Swap(a,b) {int t=a;a=b;b=t;}
+#define endl '\n'
+#define ll long long
+#define sstream stringstream
+using namespace std;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+int a[] = { 0,1,2,3,4,5,6,7,8,9,10,11 };
+
+int num = 0;
+
+
+void Perm(int begin, int end) {
+	int i;
+	if (begin == end)
+		num++;
+	else {
+		for (i = begin;i <= end;i++) {
+			Swap(a[begin], a[i]);
+			Perm(begin + 1, end);
+			Swap(a[begin], a[i]);
+		}
+	}
+}
+
+int main() {
+	clock_t s, e;
+	s = clock();
+	Perm(0, 11);
+	e = clock();
+	cout << double(e - s) / CLOCKS_PER_SEC << endl;
+	return 0;
+}
