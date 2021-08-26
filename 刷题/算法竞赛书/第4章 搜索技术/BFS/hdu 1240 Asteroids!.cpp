f6d7a21e3c11f1953fc784445d966bd15c70891b
Afool4U <2312163474@qq.com>
2021-08-27 04:25:24 +0800

    BFS真难啊

---
+++ 刷题/算法竞赛书/第4章 搜索技术/BFS/hdu 1240 Asteroids!.cpp
@@ -0,0 +1,89 @@
+﻿/**
+ * @Author hjl
+ * @Date 2021年8月27日03:59:16
+ */
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define ll long long
+#define sstream stringstream
+using namespace std;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+int N;
+char a[15][15][15];
+struct node {
+	int x, y, z;
+	bool operator<(const node& b)const {
+		if (x != b.x)
+			return x < b.x;
+		else if (y != b.y)
+			return y < b.y;
+		return z < b.z;
+	}
+	bool operator==(const node &b)const {
+		return x == b.x && y == b.y && z == b.z;
+	}
+	operator bool() const {
+		return a[x][y][z] != 'X' && x >= 0 && x < N&& y >= 0 && y < N&& z >= 0 && z < N;
+	}
+};
+
+int main() {
+	node START, END, start, next;
+	string temp;
+	while (cin >> temp >> N) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					cin >> a[i][j][k];
+				}
+			}
+		}
+		cin >> START.x >> START.y >> START.z;
+		a[START.x][START.y][START.z] = 'O';
+		cin >> END.x >> END.y >> END.z;
+		a[END.x][END.y][END.z] = 'O';
+		cin >> temp;
+
+		start = START;
+		map<node, node> M;
+		queue<node> Q;
+		Q.push(start);
+		M[start] = { -1,-1,-1 };
+		while (!Q.empty()) {
+			start = Q.front(); Q.pop();
+			if (start == END)
+				break;
+			next = start, next.x++;
+			if (next && M.find(next) == M.end()) Q.push(next), M[next] = start;
+
+			next = start, next.x--;
+			if (next && M.find(next) == M.end()) Q.push(next), M[next] = start;
+
+			next = start, next.y++;
+			if (next && M.find(next) == M.end()) Q.push(next), M[next] = start;
+
+			next = start, next.y--;
+			if (next && M.find(next) == M.end()) Q.push(next), M[next] = start;
+
+			next = start, next.z++;
+			if (next && M.find(next) == M.end()) Q.push(next), M[next] = start;
+
+			next = start, next.z--;
+			if (next && M.find(next) == M.end()) Q.push(next), M[next] = start;
+		}
+		if (start == END) {
+			int cnt = 0;
+			for (node t = END; !(t == START); t = M[t]){
+				cnt++;
+			}
+			cout << N << " " << cnt << endl;
+		}
+		else
+			cout << "NO ROUTE" <<endl;
+	}
+	return 0;
+}
---
+++ 刷题/算法竞赛书/第4章 搜索技术/BFS/poj 3414 Pots(网上的AC答案，用了map真的很巧妙).cpp
@@ -0,0 +1,65 @@
+﻿/**
+ * @Author hjl
+ * @Date 2021年8月26日04:30:53
+ */
+
+#include <bits/stdc++.h>
+
+#define Swap(a,b) {int t=a;a=b;b=t;}
+#define endl '\n'
+#define ll long long
+#define sstream stringstream
+using namespace std;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+struct Data {
+	int x, y; string s;
+	bool operator <(Data b) const {
+		if (x != b.x) return x < b.x;
+		return y < b.y;
+	}
+};
+
+int main() {
+	int A, B, C, sum;
+	Data t, temp;
+	cin >> A >> B >> C;
+	t.x = t.y = 0; temp.x = -1;
+	queue<Data> Q;
+	map<Data, Data> M;
+
+	Q.push(t); M[t] = temp;
+	while (!Q.empty()) {
+		t = Q.front(); Q.pop();
+		if (t.x == C || t.y == C) break;
+		temp.x = A; temp.y = t.y; temp.s = "FILL(1)";
+		if (M.find(temp) == M.end()) Q.push(temp), M[temp] = t;
+
+		temp.x = t.x; temp.y = B; temp.s = "FILL(2)";
+		if (M.find(temp) == M.end()) Q.push(temp), M[temp] = t;
+
+		temp.x = 0; temp.y = t.y; temp.s = "DROP(1)";
+		if (M.find(temp) == M.end()) Q.push(temp), M[temp] = t;
+
+		temp.x = t.x; temp.y = 0; temp.s = "DROP(2)";
+		if (M.find(temp) == M.end()) Q.push(temp), M[temp] = t;
+
+		temp.x = min(t.x + t.y, A);
+		temp.y = max(0, t.y - A + t.x); temp.s = "POUR(2,1)";
+		if (M.find(temp) == M.end()) Q.push(temp), M[temp] = t;
+
+		temp.x = max(0, t.x - B + t.y);
+		temp.y = min(t.x + t.y, B); temp.s = "POUR(1,2)";
+		if (M.find(temp) == M.end()) Q.push(temp), M[temp] = t;
+	}
+	if (t.x == C || t.y == C) {
+		string ans = ""; sum = 0;
+		/* 好好记忆 */
+		for (temp = t; temp.x || temp.y; temp = M[temp]) ans += temp.s + "\n", sum++;
+		cout << sum << endl << ans;
+	}
+	else cout << "impossible" << endl;
+	
+	return 0;
+}
