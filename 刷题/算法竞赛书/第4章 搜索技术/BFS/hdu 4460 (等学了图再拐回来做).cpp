fa32340e8ec1ae5f18453eb76292a9ab670980f6
Afool4U <2312163474@qq.com>
2021-08-27 14:11:34 +0800

    無

---
+++ 刷题/算法竞赛书/第4章 搜索技术/BFS/hdu 4460 (等学了图再拐回来做).cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * @Author hjl
+ * @Date 2021年8月27日13:47:08
+ */
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define ll long long
+#define sstream stringstream
+using namespace std;
+const double eps = 1e-7;
+const auto FAST = (ios::sync_with_stdio(0), cin.tie(0), cout.tie(0));
+
+struct node {
+	
+};
+
+int main() {
+	int N;
+	while (cin>>N && N) {
+
+	}
+
+
+	return 0;
+}
